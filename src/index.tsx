3eb2f338eacd089e800d0cc8e82ea24520b02a00
Robson Oliveira dos Santos <robsonos@outlook.com>
2022-11-19 09:28:06 +0930

    Initial BLE test

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import { NativeModules } from 'react-native';
 import { Counter } from './components/counter';
+import { MyriotaDFU } from './modules/myriota-ble-dfu';
 
 export {
   Counter,
+  MyriotaDFU,
 }
 
 export default NativeModules.RNMyriotBLEDFU
---
+++ src/modules/myriota-ble-dfu.tsx
@@ -0,0 +1,9 @@
+import BleManager from 'react-native-ble-manager'
+
+export const UART_SERVICE_UUID = '6E400001-B5A3-F393-E0A9-E50E24DCCA9E'
+export const RX_CHARACTERISTIC = '6E400002-B5A3-F393-E0A9-E50E24DCCA9E'
+export const TX_CHARACTERISTIC = '6E400003-B5A3-F393-E0A9-E50E24DCCA9E'
+
+export const MyriotaDFU = async () => {
+
+}
