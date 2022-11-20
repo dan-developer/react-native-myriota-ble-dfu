74e2bc0c28c3c263365fd8be6aff41fc1c211967
Robson Oliveira dos Santos <robsonos@outlook.com>
2022-11-21 08:41:07 +0930

    Improve clean test

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,8 +1,8 @@
 import RNMyriotaBLEDFUModule, { MyriotaDFU } from 'react-native-myriota-ble-dfu'
-import BleManager from 'react-native-ble-manager'
+import BleManager, { Peripheral } from 'react-native-ble-manager'
 import { PERMISSIONS, requestMultiple } from 'react-native-permissions'
 import DeviceInfo from 'react-native-device-info'
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import {
   NativeModules,
   NativeEventEmitter,
@@ -18,18 +18,7 @@ const RX_CHARACTERISTIC = '6E400002-B5A3-F393-E0A9-E50E24DCCA9E'
 const TX_CHARACTERISTIC = '6E400003-B5A3-F393-E0A9-E50E24DCCA9E'
 
 const App = () => {
-  const [isScanning, setIsScanning] = useState(false)
-
   const peripherals = new Map()
-
-  const sleep = async (ms: number) =>
-    await new Promise((success) => setTimeout(success, ms))
-
-  const handleStopScan = () => {
-    console.log('handleStopScan')
-    console.log('Scan is stopped')
-    setIsScanning(false)
-  }
 
   const handleDisconnectedPeripheral = (data) => {
     console.log('handleDisconnectedPeripheral')
@@ -50,15 +39,6 @@ const App = () => {
         data.characteristic,
       data.value
     )
-  }
-
-  const handleDiscoverPeripheral = (peripheral) => {
-    console.log('handleDiscoverPeripheral')
-    console.log('Got ble peripheral', peripheral)
-    if (!peripheral.name) {
-      peripheral.name = 'NO NAME'
-    }
-    peripherals.set(peripheral.id, peripheral)
   }
 
   const requestPermissions = (): Promise<void> => {
@@ -113,26 +93,66 @@ const App = () => {
     })
   }
 
-  const startScan = async () => {
-    console.log('startScan: starting')
-    // await sleep(5000)
-    // console.log('here2')
-    if (!isScanning) {
-      await BleManager.scan([UART_SERVICE_UUID], 15, false)
+  interface ScanBLEOptions {
+    name: string
+    serviceUUIDs: string[]
+    timeout?: number
+  }
+
+  const scanForBLE = async (
+    scanBLEOptions: ScanBLEOptions
+  ): Promise<Peripheral> => {
+    return new Promise(async (success) => {
+      console.log('startScan: starting')
+
+      const stopScanSubscription = bleManagerEmitter.addListener(
+        'BleManagerStopScan',
+        async () => {
+          console.log('startScan: stopped')
+          discoverSubscription.remove()
+          stopScanSubscription.remove()
+        }
+      )
+
+      const discoverSubscription = bleManagerEmitter.addListener(
+        'BleManagerDiscoverPeripheral',
+        async (peripheral: Peripheral) => {
+          if (
+            peripheral.name == scanBLEOptions.name ||
+            peripheral.advertising.localName == scanBLEOptions.name
+          ) {
+            await BleManager.stopScan()
+            success(peripheral)
+          }
+        }
+      )
+
+      const timeout = scanBLEOptions.timeout ? scanBLEOptions.timeout : 15
+
+      await BleManager.scan(scanBLEOptions.serviceUUIDs, timeout, false)
 
       console.log('startScan: started')
-      setIsScanning(true)
-    }
+    })
   }
 
   const stopScan = async () => {
-    console.log('stopScan: stoping')
-    await BleManager.stopScan()
-    console.log('stopScan: stopped')
-    setIsScanning(false)
+    return new Promise<void>(async (success) => {
+      console.log('stopScan: stoping')
+
+      const stopScanSubscription = bleManagerEmitter.addListener(
+        'BleManagerStopScan',
+        async () => {
+          console.log('stopScan: stopped')
+          stopScanSubscription.remove()
+          success()
+        }
+      )
+
+      await BleManager.stopScan()
+    })
   }
 
-  const startBLE = (): Promise<void> => {
+  const initBLE = (): Promise<void> => {
     return new Promise(async (success, error) => {
       console.log('startBLE: starting')
       await BleManager.start({ showAlert: false })
@@ -161,14 +181,23 @@ const App = () => {
   }
 
   useEffect(() => {
-    const init = async () => {
-      await requestPermissions()
-      await startBLE()
-      await startScan()
-    }
+    const init = async () => {}
 
-    init().catch((error) => console.error('Initialization error:', error))
-    // scanForUUID('test')
+    init()
+      .then(async () => await requestPermissions())
+      .catch((error) => console.error('Permission error:', error))
+      .then(async () => await initBLE())
+      .catch((error) => console.error('BLE initialization error:', error))
+      .then(
+        async () =>
+          await scanForBLE({
+            name: 'PLS9896B7',
+            serviceUUIDs: [UART_SERVICE_UUID],
+          })
+      )
+      .catch((error) => console.error('BLE scan error:', error))
+      .then((peripheral) => console.log('peripheral:', peripheral))
+
     // Android events
     // BleManagerDidUpdateState
     // BleManagerPeripheralDidBond
@@ -186,11 +215,6 @@ const App = () => {
 
     const subscriptions = [
       bleManagerEmitter.addListener(
-        'BleManagerDiscoverPeripheral',
-        handleDiscoverPeripheral
-      ),
-      bleManagerEmitter.addListener('BleManagerStopScan', handleStopScan),
-      bleManagerEmitter.addListener(
         'BleManagerDisconnectPeripheral',
         handleDisconnectedPeripheral
       ),
@@ -202,7 +226,12 @@ const App = () => {
 
     return () => {
       console.log('unmount')
-      stopScan()
+
+      const stop = async () => {}
+
+      stop()
+        .then(async () => await stopScan())
+        .catch((error) => console.error('BLE stopScan error:', error))
 
       subscriptions.forEach((s) => s?.remove?.())
     }
